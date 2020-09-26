decac0ee2d126d9f629926b2826fe68d030b03ca
justinplesea <pleseaj@googlemail.com>
2020-09-26 13:13:38 +0100

    CheckoutPage

---
+++ fail.js
@@ -0,0 +1,27 @@
+  window.addEventListener('DOMContentLoaded', (event) => {
+
+  var url = window.location.search;
+  var urlParams = new URLSearchParams(url);
+  var sessions_id  =  urlParams.get('cko-session-id');
+
+  var request = new XMLHttpRequest()
+
+
+  var url = "https://api.sandbox.checkout.com/payments/" + sessions_id;
+  fetch(url, {
+          method: 'GET',
+          headers: {
+              'Authorization': 'sk_test_0b9b5db6-f223-49d0-b68f-f6643dd4f808',
+          }
+      }).then(response=>response.json())
+          .then(data => { 
+            var payment_status = data.status;
+            document.getElementById("transaction_details").innerHTML = "We are sorry you card as been "+ payment_status.toLowerCase();
+          })
+          //Catching posible errors which could happend either because of request or data missing.
+          .catch(err => {
+            console.log('ERROR:', err);
+          })
+
+ 
+});
